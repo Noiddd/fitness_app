9a75455a4002e5451fae027243ed13aea1dd2ef8
Noiddd <dionang11@gmail.com>
2022-08-14 22:10:07 +0800

    36:30

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import ReactDom from "react-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
