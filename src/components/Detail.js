8aaf3598372823d95f6e33d0dbbbbcd5975daeb9
Noiddd <dionang11@gmail.com>
2022-08-15 22:41:40 +0800

    final product

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -39,7 +39,9 @@ const Detail = ({ exerciseDetail }) => {
           gap: { lg: "35px", xs: "20px" },
         }}
       >
-        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h3" textTransform="capitalize">
+          {name}
+        </Typography>
         <Typography variant="h6">
           Make it a habit! {name} is one of the best exercises to target your{" "}
           {target}. It will help you improve your mood and gain energy.{" "}
